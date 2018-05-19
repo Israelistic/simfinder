10fdb04df269226c2d86fc4ec640bcd56a98d069
Frank Vumbaca <fvumbaca@outlook.com>
2018-05-19 10:14:50 -0400

    Added more tests

--- src/io.rs
+++ src/io.rs
@@ -42,6 +42,12 @@ pub fn write_results(mut f: &File, filename0: &str, filename1: &str, similarity:
 
 #[cfg(test)]
 mod tests {
+
+	use std::path::PathBuf;
+	use std::fs::{File, remove_file};
+	use std::io::prelude::*;
+
+
 	#[test]
 	fn load_test() {
 		// This test also contains files that do not exist to also test for
@@ -62,4 +68,28 @@ mod tests {
 		let result = super::load("examples/one-too-few.csv", "./");
 		assert!(result.is_ok(), "The badly formated file should be loaded properly");
 	}
+
+	#[test]
+	fn write_test() {
+		let filename = PathBuf::from("examples/example-output.csv");
+		let filename = filename.to_str().expect("Path should evaluate properly.");
+
+		let mut file = File::create(filename).expect("Unable to open the file");
+
+		assert!(super::write_headers(&mut file).is_ok(), "File headers should be written properly.");
+
+		assert!(super::write_results(&mut file, "test1.png", "test2.png", 0.111, 111).is_ok(),
+		 "Results should be written to the output file.");
+
+		let mut file = File::open(filename).expect("Should be able to open file just written to.");
+
+    	let mut result = String::new();
+    	file.read_to_string(&mut result).expect("Unable to read the file");
+    	assert_eq!(result,
+    		"image1, image2, similar, elapsed,\ntest1.png, test2.png, 0.11, 111,\n", "The output should be exactly as expected.");
+
+    	assert!(remove_file(filename).is_ok(), "Test output was deleted correctly");
+
+	}
+
 }
